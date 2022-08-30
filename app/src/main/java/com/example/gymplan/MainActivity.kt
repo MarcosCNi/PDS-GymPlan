6959ed08435cb57be672863858f946b7392ff005
Marcos Nicolau <marcos.nicolau.external@tkelevator.com>
2022-08-30 10:37:48 -0300

    Update on design colors

--- app/src/main/java/com/example/gymplan/MainActivity.kt
+++ app/src/main/java/com/example/gymplan/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.gymplan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.gymplan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment =  supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomAppBar.apply {
+            setupWithNavController(navController)
+        }
     }
 }
